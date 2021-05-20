0682a36b7f3030cab770e48c6d7e44ac07572f21
Roy Vardi <vardiroy4@gmail.com>
2021-05-20 07:23:56 +0300

    finish home page

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -9,10 +9,25 @@ import PeopleIcon from '@material-ui/icons/People';
 import ReceiptIcon from '@material-ui/icons/Receipt';
 import ColorizeIcon from '@material-ui/icons/Colorize';
 import {GiPistolGun} from 'react-icons/gi'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
+import Divider from '@material-ui/core/Divider'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100vh',
+  },
+  list: {
+    width: '80%',
+    height: '50%',
+    marginRight: theme.spacing(3),
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
   },
   background: {
     backgroundColor: "#fde85e"
@@ -53,19 +68,83 @@ export default function Home({userData}) {
 
   return (
     <Grid container component="main" className={classes.root}>
-      <Grid className={classes.background} item xs={12} sm={8} component={Paper} elevation={6} square>
-        <h1>שלום, {userData.name}</h1>
-        <MySpeedDial/>
+      <Grid className={classes.background} xs={12} component={Paper} elevation={6} square>
+          <h1>שלום, {userData.name}</h1>
+          <Grid container 
+                elevation={6}
+                direction="row"
+                alignItems='center'
+                justify="center"> 
+            <MyList userData={userData}/>
+            <MySpeedDial/>
+          </Grid>
       </Grid>  
     </Grid>
   );
 }
 
 
+const MyList = ({userData}) => {
+    const classes = useStyles();
+
+    const isOverDue = (date) => {
+        const nowDate = new Date()
+        const inputDate = new Date(date)
+        return inputDate < nowDate 
+    }
+
+    const dateFormatter = (date) => {
+        const newDate = new Date(date)
+        console.log(newDate)
+        return `${newDate.getDate()}.${newDate.getMonth() + 1}.${newDate.getFullYear()}`
+    }
+
+    const filterRanges = (ranges) => {
+        return ranges.sort((r1, r2) => {
+            const nr1 = new Date(r1.date)
+            const nr2 = new Date(r2.date)
+            return nr1 < nr2 ? 1 : -1 
+        })
+    }
+
+    console.table(filterRanges(userData.ranges))
+
+    return (
+        <List className={classes.list}>
+        {filterRanges(userData.ranges).map((range) => {
+            return (
+            <>
+            <ListItem key={range.name}  button>
+                <ListItemAvatar>
+                    <Avatar>
+                        <GiPistolGun/>
+                    </Avatar>
+                </ListItemAvatar>
+                <ListItemText secondary={`תאריך: ${dateFormatter(range.date)}`} primary="מטווח" />
+                <br/>
+                <br/>
+                <br/>
+                <ListItemSecondaryAction>
+                {   isOverDue(range.date) ?
+                    <Chip
+                        label="פג תוקף"
+                        color="secondary"
+                        variant="outlined"
+                    /> : null
+                }
+                </ListItemSecondaryAction>
+            </ListItem>
+            <Divider/>
+            </>
+            );
+        })}
+        </List>
+    )
+}
+
 const MySpeedDial = () => {
     const classes = useStyles();
     const [open, setOpen] = useState(false);
-    const [hidden, setHidden] = useState(false);
 
     const handleClose = () => {
     setOpen(false);
@@ -79,7 +158,6 @@ const MySpeedDial = () => {
         <SpeedDial
             ariaLabel="SpeedDial example"
             className={classes.speedDial}
-            hidden={hidden}
             icon={<SpeedDialIcon />}
             onClose={handleClose}
             onOpen={handleOpen}
