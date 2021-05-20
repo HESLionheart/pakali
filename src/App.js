a11dbf58ea4fc8b064092b973e67561e6cdd09d3
Roy Vardi <vardiroy4@gmail.com>
2021-05-20 03:26:44 +0300

    init home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { create } from "jss";
 import rtl from "jss-rtl";
 import {
@@ -8,6 +8,7 @@ import {
   createMuiTheme
 } from "@material-ui/core/styles";
 import Login from './Views/Login'
+import Home from './Views/Home'
 
 // Configure JSS
 const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
@@ -15,13 +16,22 @@ const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
 const rtlTheme = createMuiTheme({ direction: "rtl" })
 
 function AppContent() {
+
+  const [userData, setUserData] = useState({})
+
+  const updateUserData = (data) => {
+    setUserData(data)
+  }
+
   useEffect(() => {
     document.body.setAttribute("dir", "rtl");
   }, [])
 
   return (
     <ThemeProvider theme={rtlTheme}>
-      <Login/>
+      {
+        userData.tz ? <Home userData={userData}/> : <Login updateUserData={updateUserData}/>
+      }
     </ThemeProvider>
   );
 }
--- src/Views/Home.js
+++ src/Views/Home.js
@@ -2,13 +2,21 @@ import React, {useState} from 'react'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
+import SpeedDial from '@material-ui/lab/SpeedDial';
+import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
+import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
+import FileCopyIcon from '@material-ui/icons/FileCopyOutlined';
+import SaveIcon from '@material-ui/icons/Save';
+import PrintIcon from '@material-ui/icons/Print';
+import ShareIcon from '@material-ui/icons/Share';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100vh',
   },
   background: {
-    backgroundColor: "#fde85e",
+    backgroundColor: "#fde85e"
   },
   paper: {
     margin: theme.spacing(8, 4),
@@ -21,44 +29,68 @@ const useStyles = makeStyles((theme) => ({
     height: "180px",
     margin: theme.spacing(1),
   },
+  speedDial: {
+    position: 'absolute',
+    '&.MuiSpeedDial-directionUp, &.MuiSpeedDial-directionLeft': {
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+    },
+    '&.MuiSpeedDial-directionDown, &.MuiSpeedDial-directionRight': {
+      top: theme.spacing(2),
+      left: theme.spacing(2),
+    },
+  },
 }));
 
-export default function SignInSide() {
+const actions = [
+    { icon: <FileCopyIcon />, name: 'Copy' },
+    { icon: <SaveIcon />, name: 'Save' },
+    { icon: <PrintIcon />, name: 'Print' },
+    { icon: <ShareIcon />, name: 'Share' },
+    { icon: <FavoriteIcon />, name: 'Like' },
+  ];
+
+export default function Home({userData}) {
   const classes = useStyles();
 
-  const [isLoading, setIsLoading] = useState(false)
-  const [input, setInput] = useState({})
+//   const [isLoading, setIsLoading] = useState(false)
 
-  const handleInputChange = (e) => setInput({
-    ...input,
-    [e.currentTarget.name]: e.currentTarget.value
-  })
+    const [open, setOpen] = React.useState(false);
+    const [hidden, setHidden] = React.useState(false);
 
-  const handleSubmit = async (e) => {
-    setIsLoading(true)  
-    const response = await authUser(input.id, input.password)
-    setTimeout(() => {
-      setIsLoading(false)
-      if(response) {
-        switch (response.status) {
-          case 200:
-            alert("yeshhhhhhhhhh")
-            break;
-          case 401:
-            setIsWrongPassword(true)
-            break;
-        }
-      } else {
-        alert("שגיאה לא צפויה, נסו שוב מאוחר יותר")
-      }
-    }, 1000)
-  }
+    const handleClose = () => {
+    setOpen(false);
+    };
+
+    const handleOpen = () => {
+    setOpen(true);
+    };
 
   return (
     <Grid container component="main" className={classes.root}>
       <Grid className={classes.background} item xs={12} sm={8} component={Paper} elevation={6} square>
-        Home page
-      </Grid>
+        <h1>שלום, {userData.Name}</h1>
+        <SpeedDial
+          ariaLabel="SpeedDial example"
+          className={classes.speedDial}
+          hidden={hidden}
+          icon={<SpeedDialIcon />}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          open={open}
+          direction="up"
+        >
+          {actions.map((action) => (
+            <SpeedDialAction
+              key={action.name}
+              icon={action.icon}
+              tooltipTitle={action.name}
+              onClick={handleClose}
+            />
+          ))}
+        </SpeedDial>
+      </Grid>  
     </Grid>
   );
 }
+
